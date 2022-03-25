62824fcc8aac97ed6dfaecc1ebdf513b7b62768b
Junior Souza <jr.souza9520@gmail.com>
2022-03-25 13:55:17 -0300

    Update GetNotesCombinationUseCase.cs

--- src/DesafioItau.Application/UseCases/V1/CashWithdrawal/GetNotesCombination/GetNotesCombinationUseCase.cs
+++ src/DesafioItau.Application/UseCases/V1/CashWithdrawal/GetNotesCombination/GetNotesCombinationUseCase.cs
@@ -1,14 +1,18 @@
 ﻿using DesafioItau.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace DesafioItau.Application.UseCases.V1.CashWithdrawal.GetNotesCombination;
 
 public class GetNotesCombinationUseCase : IGetNotesCombinationUseCase
 {
     private readonly IBanknoteRepository _repository;
+    private readonly ILogger<GetNotesCombinationUseCase> _logger;
 
-    public GetNotesCombinationUseCase(IBanknoteRepository repository)
+    public GetNotesCombinationUseCase(IBanknoteRepository repository,
+                                      ILogger<GetNotesCombinationUseCase> logger)
     {
         _repository = repository;
+        _logger = logger;
     }
 
     public IEnumerable<GetNotesCombinationResponse> GetNotesCombination(GetNotesCombinationRequest request)
